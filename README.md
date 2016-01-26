c345695f630041eef4e45416cddeae67d0590564
NishanthSpShetty <nishanthspshetty@gmail.com>
2016-01-26 17:53:04 +0530

    implementation of commands

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # boot_shell
 
 A simple boot shell written in assembly.
+Commands availabe
+	help : display the list of commands availabe
+	echo : just print the hello message
+	1    : clears the screen
--- boot_shell.asm
+++ boot_shell.asm
@@ -5,46 +5,28 @@ org 0x7c00
 bits 16
 start:
 	call clr_screen
-	mov si,welcome
-	call print
-	
- 	mov cl,9
- while:
-	call print_newline
-
+ 	mov cl,10 ;max loop 10 times
+while:
 	mov si,prompt
 	call print
 	call gets
-	call print_newline
-	mov si,you_entered
-	call print
-	mov si,input_buffer
 	mov byte [si+BUFFER_SIZE-1],0
-	call print
-	
-	call clear_input_buffer
-	
+	mov si,input_buffer
 	call print_newline
+	pusha	
+	call cmd_help
+	call cmd_clr
+	call cmd_echo
+	popa
+	call clear_input_buffer
 	loop while
 
 	mov si,press_any
 	call print
 	call getch
-	call print_newline
-	call print_newline
-	call print_newline
-	call print_newline
-	call print_newline
 	mov si,bye
 	call print
-	call getch
-	call clr_screen
-	;halt the system
 	hlt
-	cli
-	mov al,01h
-	mov ah,42h
-	int 15h
 
 gets:
 	mov si,input_buffer
@@ -73,6 +55,8 @@ clr_screen:
 	mov ah,0x00
 	mov al,0x03
 	int 10h
+	mov si,welcome
+	call print
 	ret
 putchar:
 	mov ah,0x0E
@@ -113,13 +97,108 @@ clear_input_buffer:
 	popa
 	ret
 
-; data section
-you_entered : db "You entered  : ",0
-input_buffer : times BUFFER_SIZE db 0; padd with 0 
-welcome :  db '*************Welcome to boot shell*************',0;
+;to_bcd_print:
+;	push dx
+;	push ax
+;	 mov ah,al
+;	 shr ah,1
+;;	 shr ah,1
+;	 shr ah,1
+;;	 shr ah,1
+;	 and al,0fh  ; Mask off low digit.
+;	 or ax,3030H; Convert binary digits to ASCII characters.
+;       push ax
+;       mov al,ah
+;	call putchar
+;	pop ax
+;	call putchar
+;	pop ax
+;	pop dx
+;	ret
+	
+
+;time:
+;	pusha
+;	mov ah,02h
+;	int 1Ah
+;	mov ax,0x00
+;	mov al,ch; hour
+;	call to_bcd_print
+;	
+;	mov al,'/'
+;	call putchar
+	
+;	mov al,cl ;min
+;	call to_bcd_print	
+;	popa
+;	ret
+
+;exec_cmd:
+;	pusha
+;	call cmd_time
+;	call cmd_clr
+;	call cmd_echo
+;	call cmd_help
+;	popa
+;	ret
+cmd_help:
+	cmp byte [si],'h'
+	jne not_help
+	inc si
+	cmp byte [si],'e'
+	jne not_help
+	inc si
+	cmp byte [si],'l' 
+	jne not_help
+	inc si
+	cmp byte [si],'p'
+	jne not_help
+	mov si,help
+	call print
+;	jmp clr_executed
+	not_help:
+;		mov byte [invalid],1
+;	clr_executed:
+	ret
+
+cmd_clr:
+	cmp byte [si],'0'
+	jne not_clr
+	call clr_screen
+	not_clr:
+		ret
+cmd_echo:
+	cmp byte [si],'e'
+	jne not_echo
+	inc si
+	cmp byte [si],'c'
+	jne not_echo
+	inc si
+	cmp byte [si],'h'
+	jne not_echo
+	inc si
+	cmp byte [si],'o'
+	jne not_echo
+	mov si,echo
+	call print
+;	jmp echo_executed
+
+	not_echo :
+;		mov byte [invalid],1
+;	echo_executed:
+		ret
+
+
+
+welcome :  db '______________Welcome to boot shell_______________',10,13,0
 prompt : db 'root@boot_shell >',0
-press_any : db 'Press any key to continue',0;
-bye : db "Thank you for using me...good bye",0
+press_any : db 'Press any key to continue',0
+echo : db "Hello... how can i help you sir ? ",13,10,0
+bye : db 10,13,"Thank you for using me...good bye",0
+help : db "boot shell commands",13,10," echo",13,10," help",13,10,' ',49," : clear screen",13,10,0
+input_buffer : times BUFFER_SIZE db 0; padd with 0 
+;err : db "Invalid command",0
+;invalid : db 1
 times 510-($-$$) db 0
 
 dw 0xAA55
